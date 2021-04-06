d878c5948f8c3194775fc5ae0ed9f437cf606594
ssseongeunkim <ourment@naver.com>
2021-04-06 17:51:34 +0900

    v01

--- app/src/main/java/com/seong/pms/App.java
+++ app/src/main/java/com/seong/pms/App.java
@@ -16,152 +16,159 @@ public class App {
       int menu = Prompt.inputInt("[문서관리 시스템]\n 1. 문서입력\n 2. 문서조회\n 3. 문서수정\n 4. 문서삭제\n "
           + "99. 종료\n > ");
 
-      switch (menu) {
-        case 1:
-          menu = Prompt.inputInt("\n[문서관리 시스템/문서입력]\n "
-              + "1. 사원정보 입력\n 2. 휴가신청서 작성\n 3. 퇴사신청서 작성\n 90. 뒤로가기\n 99. 종료\n > ");
+      try {
+        switch (menu) {
+          case 1:
+            menu = Prompt.inputInt("\n[문서관리 시스템/문서입력]\n "
+                + "1. 사원정보 입력\n 2. 휴가신청서 작성\n 3. 퇴사신청서 작성\n 90. 뒤로가기\n 99. 종료\n > ");
 
-          switch (menu) {
-            case 1:
-              memberHandler.add();
-              break;
-            case 2:
-              vacationHandler.add();
-              break;
-            case 3:
-              resignationHandler.add();
-              break;
-            case 90:
-              System.out.println("\n뒤로갑니다.\n");
-              continue;
-            case 99:
-              System.out.println("\n시스템을 종료합니다.");
-              return;
-            default:
-              System.out.println("\n없는 메뉴입니다.\n");
-          }
+            switch (menu) {
+              case 1:
+                memberHandler.add();
+                break;
+              case 2:
+                vacationHandler.add();
+                break;
+              case 3:
+                resignationHandler.add();
+                break;
+              case 90:
+                System.out.println("\n뒤로갑니다.\n");
+                continue;
+              case 99:
+                System.out.println("\n시스템을 종료합니다.");
+                return;
+              default:
+                System.out.println("\n없는 메뉴입니다.\n");
+            }
 
-          break;
-        case 2:
-          menu = Prompt.inputInt("\n[문서관리 시스템/문서조회]\n "
-              + "1. 전체조회\n 2. 선택조회\n 90. 뒤로가기\n 99. 종료\n > ");
+            break;
+          case 2:
+            menu = Prompt.inputInt("\n[문서관리 시스템/문서조회]\n "
+                + "1. 전체조회\n 2. 선택조회\n 90. 뒤로가기\n 99. 종료\n > ");
 
-          switch (menu) {
-            case 1:
-              menu = Prompt.inputInt("\n[문서관리 시스템/문서조회/전체조회]\n "
-                  + "1. 사원정보 조회\n 2. 휴가신청서 조회\n 3. 퇴사신청서 조회\n 90. 뒤로가기\n 99. 종료\n > ");
+            switch (menu) {
+              case 1:
+                menu = Prompt.inputInt("\n[문서관리 시스템/문서조회/전체조회]\n "
+                    + "1. 사원정보 조회\n 2. 휴가신청서 조회\n 3. 퇴사신청서 조회\n 90. 뒤로가기\n 99. 종료\n > ");
 
-              switch (menu) {
-                case 1:
-                  memberHandler.list();
-                  break;
-                case 2:
-                  vacationHandler.list();
-                  break;
-                case 3:
-                  resignationHandler.list();
-                  break;
-                case 90:
-                  System.out.println("\n뒤로갑니다.\n");
-                  continue;
-                case 99:
-                  System.out.println("\n시스템을 종료합니다.");
-                  return;
-                default:
-                  System.out.println("\n없는 메뉴입니다.\n");
-              }
+                switch (menu) {
+                  case 1:
+                    memberHandler.list();
+                    break;
+                  case 2:
+                    vacationHandler.list();
+                    break;
+                  case 3:
+                    resignationHandler.list();
+                    break;
+                  case 90:
+                    System.out.println("\n뒤로갑니다.\n");
+                    continue;
+                  case 99:
+                    System.out.println("\n시스템을 종료합니다.");
+                    return;
+                  default:
+                    System.out.println("\n없는 메뉴입니다.\n");
+                }
 
-              break;
-            case 2:
-              menu = Prompt.inputInt("\n[문서관리 시스템/문서조회/선택조회]\n "
-                  + "1. 사원정보 조회\n 2. 휴가신청서 조회\n 3. 퇴사신청서 조회\n 90. 뒤로가기\n 99. 종료\n > ");
+                break;
+              case 2:
+                menu = Prompt.inputInt("\n[문서관리 시스템/문서조회/선택조회]\n "
+                    + "1. 사원정보 조회\n 2. 휴가신청서 조회\n 3. 퇴사신청서 조회\n 90. 뒤로가기\n 99. 종료\n > ");
 
-              switch (menu) {
-                case 1:
-                  memberHandler.detail();
-                  break;
-                case 2:
-                  vacationHandler.detail();
-                  break;
-                case 3:
-                  resignationHandler.detail();
-                  break;
-                case 90:
-                  System.out.println("\n뒤로갑니다.\n");
-                  continue;
-                case 99:
-                  System.out.println("\n시스템을 종료합니다.");
-                  return;
-                default:
-                  System.out.println("\n없는 메뉴입니다.\n");
-              }
+                switch (menu) {
+                  case 1:
+                    memberHandler.detail();
+                    break;
+                  case 2:
+                    vacationHandler.detail();
+                    break;
+                  case 3:
+                    resignationHandler.detail();
+                    break;
+                  case 90:
+                    System.out.println("\n뒤로갑니다.\n");
+                    continue;
+                  case 99:
+                    System.out.println("\n시스템을 종료합니다.");
+                    return;
+                  default:
+                    System.out.println("\n없는 메뉴입니다.\n");
+                }
 
-              break;
-            case 90:
-              System.out.println("\n뒤로갑니다.\n");
-              continue;
-            case 99:
-              System.out.println("\n시스템을 종료합니다.");
-              return;
-            default:
-              System.out.println("\n없는 메뉴입니다.\n");
-          }
+                break;
+              case 90:
+                System.out.println("\n뒤로갑니다.\n");
+                continue;
+              case 99:
+                System.out.println("\n시스템을 종료합니다.");
+                return;
+              default:
+                System.out.println("\n없는 메뉴입니다.\n");
+            }
 
-          break;
-        case 3:
-          menu = Prompt.inputInt("\n[문서관리 시스템/문서수정]\n "
-              + "1. 사원정보 수정\n 2. 휴가신청서 수정\n 3. 퇴사신청서 수정\n 90. 뒤로가기\n 99. 종료\n > ");
+            break;
+          case 3:
+            menu = Prompt.inputInt("\n[문서관리 시스템/문서수정]\n "
+                + "1. 사원정보 수정\n 2. 휴가신청서 수정\n 3. 퇴사신청서 수정\n 90. 뒤로가기\n 99. 종료\n > ");
 
-          switch (menu) {
-            case 1:
-              memberHandler.update();
-              break;
-            case 2:
-              vacationHandler.update();
-              break;
-            case 3:
-              resignationHandler.update();
-              break;
-            case 90:
-              System.out.println("\n뒤로갑니다.\n");
-              continue;
-            case 99:
-              System.out.println("\n시스템을 종료합니다.");
-              return;
-            default:
-              System.out.println("\n없는 메뉴입니다.\n");
-          }
+            switch (menu) {
+              case 1:
+                memberHandler.update();
+                break;
+              case 2:
+                vacationHandler.update();
+                break;
+              case 3:
+                resignationHandler.update();
+                break;
+              case 90:
+                System.out.println("\n뒤로갑니다.\n");
+                continue;
+              case 99:
+                System.out.println("\n시스템을 종료합니다.");
+                return;
+              default:
+                System.out.println("\n없는 메뉴입니다.\n");
+            }
 
-          break;
-        case 4:
-          menu = Prompt.inputInt("\n[문서관리 시스템/문서삭제]\n "
-              + "1. 사원정보 삭제\n 2. 휴가신청서 삭제\n 3. 퇴사신청서 삭제\n 90. 뒤로가기\n 99. 종료\n > ");
+            break;
+          case 4:
+            menu = Prompt.inputInt("\n[문서관리 시스템/문서삭제]\n "
+                + "1. 사원정보 삭제\n 2. 휴가신청서 삭제\n 3. 퇴사신청서 삭제\n 90. 뒤로가기\n 99. 종료\n > ");
 
-          switch (menu) {
-            case 1:
-              memberHandler.delete();
-              break;
-            case 2:
-              vacationHandler.delete();
-              break;
-            case 3:
-              resignationHandler.delete();
-              break;
-            case 90:
-              System.out.println("\n뒤로갑니다.\n");
-              continue;
-            case 99:
-              System.out.println("\n시스템을 종료합니다.");
-              return;
-            default:
-              System.out.println("\n없는 메뉴입니다.\n");
-          }
-          break;
-        case 99:
-          System.out.println("\n시스템을 종료합니다.");
-          return;
-        default:
-          System.out.println("\n없는 메뉴입니다.\n");
+            switch (menu) {
+              case 1:
+                memberHandler.delete();
+                break;
+              case 2:
+                vacationHandler.delete();
+                break;
+              case 3:
+                resignationHandler.delete();
+                break;
+              case 90:
+                System.out.println("\n뒤로갑니다.\n");
+                continue;
+              case 99:
+                System.out.println("\n시스템을 종료합니다.");
+                return;
+              default:
+                System.out.println("\n없는 메뉴입니다.\n");
+            }
+            break;
+          case 99:
+            System.out.println("\n시스템을 종료합니다.");
+            return;
+          default:
+            System.out.println("\n없는 메뉴입니다.\n");
+        }
+      } catch (Exception e) {
+        System.out.println("------------------------------------------");
+        System.out.printf("명령어 실행 중 오류 발생: %s - %s\n", 
+            e.getClass().getName(), e.getMessage());
+        System.out.println("------------------------------------------");
       }
 
       //      Prompt.close();
